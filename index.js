5fca3f28f48fa417f1866b4b3f5fec4b7560a893
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-09-29 19:25:49 -0700

    Implement fetchCoordsByIP

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+// fetchCoordsByIP('207.6.12.65', (error, coords) => {
+//    if (error) {
+//     return console.log("Error: " , error);
+//   }
+//   console.log('Coordinates: ' , coords);
+// })
--- iss.js
+++ iss.js
@@ -1,1 +1,52 @@
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+const request = require('request');
+
+const fetchMyIP = function(callback) {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    
+    if (error) {
+      return callback(error, null);
+    }
+    
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const ip = JSON.parse(body).ip;
+    return callback(error, ip);
+
+  });
+
+};
+
+const fetchCoordsByIP = function(ip, callback) {
+
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
+    
+    if (error) {
+      return callback(error, null);
+    }
+
+    const parsed = JSON.parse(body);
+    if (!parsed.success) {
+      return callback(`Mission failed. We'll get 'em next time (Success = ${parsed.success}, ${parsed.message})`, null);
+    }
+    
+    const { latitude, longitude } = parsed;
+    callback(error, {latitude, longitude});
+  });
+
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
 
